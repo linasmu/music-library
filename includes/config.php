61c3f22366849552a333edac867db9279af01029
linasmu <linas.murauskas@gmail.com>
2018-05-20 23:13:23 +0300

    connection to DB

---
+++ includes/config.php
@@ -0,0 +1,10 @@
+<?php 
+	ob_start();
+
+	$timezone = date_default_timezone_set("Europe/Vilnius");
+	$con = mysqli_connect("localhost", "root", "root","music-library");
+	if(mysqli_connect_errno()) {
+		echo "Failed to connect"." ". mysqli_connect_errno();
+	}
+
+ ?>
