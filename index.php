859c5af71fa5a12bb42957c9d652900e64941615
linasmu <linas.murauskas@gmail.com>
2018-06-13 17:19:28 +0300

    left navbar

--- index.php
+++ index.php
@@ -23,6 +23,36 @@ if(isset($_SESSION['userLoggedIn'])) {
     <title>Wellcome to Your Music!</title>
 </head>
 <body>
+
+    <div id="mainContainer">
+        <div id="topContainer">
+            <div id="navBarContainer">
+                <nav class="navBar">
+                    <a href="index.php" class="logo">
+                     <i class="fas fa-headphones"></i>
+                    </a>
+                    <div class="group">
+                        <div class="navItem">
+                            <a href="search.php" class="navItemLink">Search
+                            <i class="fas fa-search" alt="Search"></i>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="group">
+                        <div class="navItem">
+                            <a href="browse.php" class="navItemLink">Browse</a>
+                        </div>
+                        <div class="navItem">
+                            <a href="yourmusic.php" class="navItemLink">Your music</a>
+                        </div>
+                        <div class="navItem">
+                            <a href="profile.php" class="navItemLink">Freddie Mercury</a>
+                        </div>
+                    </div>
+                </nav>
+            </div>
+        </div>
+    
     <div id="nowPlayingBarContainer">
         <div id=nowPlayingBar>
             <div id="nowPlayingLeft">
@@ -90,6 +120,7 @@ if(isset($_SESSION['userLoggedIn'])) {
             </div>
 
         </div>
+    </div>
     </div>
 </body>
 </html>
