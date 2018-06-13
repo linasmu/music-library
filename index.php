f7d517c9910eeaeb74054c0532935db155dbeb7a
linasmu <linas.murauskas@gmail.com>
2018-06-13 15:45:48 +0300

    volume control

--- index.php
+++ index.php
@@ -77,7 +77,16 @@ if(isset($_SESSION['userLoggedIn'])) {
             </div>
 
             <div id="nowPlayingRight">
-
+                <div class="volumeBar">
+                    <button class="controlButton volume" title="Volume">
+                    <i class="fas fa-volume-up" alt="Volume"></i>
+                    </button>
+                    <div class="progressBar">
+                        <div class="progressBarBg">
+                            <div class="progress"></div>
+                        </div>
+                    </div>
+                </div>
             </div>
 
         </div>
