070b52f02880c1a6a8929e68008b1af2e31453cb
linasmu <linas.murauskas@gmail.com>
2018-06-14 17:11:48 +0300

    track listing

--- includes/classes/Song.php
+++ includes/classes/Song.php
@@ -13,16 +13,14 @@
         public function __construct($con, $id) {
             $this->con = $con;
             $this->id = $id;
-        }
-
-        public function getName() {
-            $query = mysqli_query($this->con, "SELECT songs FROM artists WHERE id='$this->id'");
+        
+            $query = mysqli_query($this->con, "SELECT * FROM songs WHERE id='$this->id'");
             $this->mysqliData = mysqli_fetch_array($query);
             $this->title = $this->mysqliData['title'];
             $this->artistId = $this->mysqliData['artist'];
             $this->albumId = $this->mysqliData['album'];
             $this->genre = $this->mysqliData['genre'];
-            $this->diration = $this->mysqliData['duration'];
+            $this->duration = $this->mysqliData['duration'];
             $this->link = $this->mysqliData['link'];
 
         }
