7f8236cbc093ad624144c5be38cdc1431bd90d73
linasmu <linas.murauskas@gmail.com>
2018-06-14 11:42:07 +0300

    album query building

--- index.php
+++ index.php
@@ -1,3 +1,22 @@
 <?php include("includes/header.php"); ?>
+
+<h1 class="pageHeading">Choose music you like</h1>
+
+<div class="gridViewContainer">
+    <?php
+        $albumQuery = mysqli_query($con, "SELECT * FROM albums ORDER BY RAND() LIMIT 10");
+
+        while($row = mysqli_fetch_array($albumQuery)) {
+
+            echo "<div class='gridViewItem'>
+                    <img src='". $row['label'] ."'>
+                    <div class='gridViewInfo'>
+                    ". $row['title'] ."
+                    </div>
+                </div>";
+        }
+    ?>
+</div>
+
 <?php include("includes/footer.php"); ?>
  
