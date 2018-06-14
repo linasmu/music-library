33bea8a36ae659a494f369ac449f7ed1de4dd068
linasmu <linas.murauskas@gmail.com>
2018-06-14 11:18:19 +0300

    file structure changed

---
+++ includes/header.php
@@ -0,0 +1,31 @@
+<?php
+include("includes/config.php");
+
+// session_destroy();
+
+if(isset($_SESSION['userLoggedIn'])) {
+    $userLoggedIn = $_SESSION['userLoggedIn'];
+} else {
+    header("Location: register.php");
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css" type="text/css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script> 
+	<script src="js/register.js"></script>
+    <title>Wellcome to Your Music!</title>
+</head>
+<body>
+
+    <div id="mainContainer">
+        <div id="topContainer">
+            <?php include("includes/navBarContainer.php"); ?>
+            <div class="mainViewContainer">
+                <div class="mainContent">
