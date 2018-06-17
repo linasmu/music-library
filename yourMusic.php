f49e276b74092941280ac7659308ba307e00e277
linasmu <linas.murauskas@gmail.com>
2018-06-17 15:13:20 +0300

    playlists page

---
+++ yourMusic.php
@@ -0,0 +1,10 @@
+<?php
+include("includes/includedFiles.php");
+?>
+
+<div class="playlistsContainer">
+    <h2>PLAYLISTS</h2>
+    <div class="buttonItems">
+        <button class="button green">NEW PLAYLIST</button>
+    </div>
+</div>
