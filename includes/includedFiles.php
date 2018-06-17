72a80f936c1185acc7dc26a704e1c5c940402aac
linasmu <linas.murauskas@gmail.com>
2018-06-17 20:09:11 +0300

    User class

--- includes/includedFiles.php
+++ includes/includedFiles.php
@@ -1,10 +1,18 @@
 <?php
 if(isset($_SERVER['HTTP_X_REQUESTED_WITH'])) {
     include("includes/config.php");
+    include("includes/classes/User.php");
     include("includes/classes/Artist.php");
     include("includes/classes/Album.php");
     include("includes/classes/Song.php");
-    // echo "CAME FROM AJAX";
+    
+    if(isset($_GET['userLoggedIn'])) {
+        $userLoggedIn = new User($con, $GET['userLoggedIn']);
+    } else {
+        echo "Username variable was not passed into page. Check the openPage JS function";
+        exit();
+    }
+
 } else {
     include("includes/header.php");
     include("includes/footer.php");
