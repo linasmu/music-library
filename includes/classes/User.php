7cd03737cd7075f416c50842303b4822e04c4f40
linasmu <linas.murauskas@gmail.com>
2018-06-17 21:01:47 +0300

    playlist query

--- includes/classes/User.php
+++ includes/classes/User.php
@@ -8,7 +8,11 @@
 		public function __construct($con, $username) {
 			$this->con = $con;
             $this->username = $username;
-		}
+        }
+        
+        public function getUsername() {
+            return $this->username;
+        }
 				
 	}
 
--- yourMusic.php
+++ yourMusic.php
@@ -8,5 +8,28 @@ include("includes/includedFiles.php");
         <div class="buttonItems">
             <button class="button green" onclick="createPlaylist()">NEW PLAYLIST</button>
         </div>
+
+        <?php
+            $username = $userLoggedIn->getUsername();
+            $playlistQuery = mysqli_query($con, "SELECT * FROM playlist WHERE owner='$username'");
+
+            if(mysqli_num_rows($playlistQuery) == 0) {
+                echo "<span class='noResults'>You don't have any playlists yet.</span>";
+            }
+
+            while($row = mysqli_fetch_array($playlistQuery)) {
+
+                echo "<div class='gridViewItem'>
+
+                        <div playlistImage>
+                        <i class='fas fa-music'></i>
+                        </div>
+
+                        <div class='gridViewInfo'>
+                        ". $row['name'] ."
+                        </div>
+                    </div>";
+            }
+        ?>
     </div>
 </div>
