fcf6e7d5ac6fb2446950b369d1e183c4bd7911ab
linasmu <linas.murauskas@gmail.com>
2018-06-27 21:29:33 +0300

    logout and user details

--- js/script.js
+++ js/script.js
@@ -40,6 +40,12 @@ $(document).on("change", "select.playlist", function() {
     });
 });
 
+function logout() {
+    $.post("includes/handlers/ajax/logout.php", function() {
+        location.reload();
+    });
+}
+
 function openPage(url) {
 
     if(timer != null) {
